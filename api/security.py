a14eb8789de48000588cc975fccf37d143d14883
Aayu0405 <dakua.aayushman06@gmail.com>
2026-01-20 18:41:09 +0530

    Create security.py

---
+++ api/security.py
@@ -0,0 +1,6 @@
+from fastapi import Header, HTTPException
+from api.core.config import API_KEY
+
+def verify_api_key(x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
